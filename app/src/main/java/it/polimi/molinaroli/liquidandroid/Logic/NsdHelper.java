a9ec9b19b523f71f8d7f30ce6ad6c31ef183d130
Marco Molinaroli <ma.molinaroli@gmail.com>
2017-03-08 13:10:34 +0100

    corretto nsd helper, messa icona

--- app/src/main/java/it/polimi/molinaroli/liquidandroid/Logic/NsdHelper.java
+++ app/src/main/java/it/polimi/molinaroli/liquidandroid/Logic/NsdHelper.java
@@ -19,7 +19,11 @@ package it.polimi.molinaroli.liquidandroid.Logic;
 import android.content.Context;
 import android.net.nsd.NsdServiceInfo;
 import android.net.nsd.NsdManager;
+import android.os.Build;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.jaredrummler.android.device.DeviceName;
 
 import java.util.ArrayList;
 
@@ -36,23 +40,28 @@ public class NsdHelper {
     NsdServiceInfo mService;
     String registeredName;
     private ArrayList<NsdServiceInfo> services;
+
     public NsdHelper(Context context) {
         mContext = context;
         mNsdManager = (NsdManager) context.getSystemService(Context.NSD_SERVICE);
         services = new ArrayList<>();
+        mServiceName ="Liquid " + Build.ID;
         setInit(2);
     }
+
     public void initializeNsd() {
         initializeResolveListener();
         //mNsdManager.init(mContext.getMainLooper(), this);
         setInit(2);
     }
+
     public void initializeDiscoveryListener() {
         mDiscoveryListener = new NsdManager.DiscoveryListener() {
             @Override
             public void onDiscoveryStarted(String regType) {
                 Log.d(TAG, "Service discovery started");
             }
+
             @Override
             public void onServiceFound(NsdServiceInfo service) {
                 Log.d(TAG, "Service discovery success" + service);
@@ -60,10 +69,11 @@ public class NsdHelper {
                     Log.d(TAG, "Unknown Service Type: " + service.getServiceType());
                 } else if (service.getServiceName().equals(registeredName)) {
                     Log.d(TAG, "Same machine: " + registeredName);
-                } else if (service.getServiceName().contains(mServiceName)){
-                    mNsdManager.resolveService(service, mResolveListener);
+                } else if (service.getServiceType().equals(SERVICE_TYPE)) {
+                    mNsdManager.resolveService(service, new CustomResolveListener());
                 }
             }
+
             @Override
             public void onServiceLost(NsdServiceInfo service) {
                 Log.e(TAG, "service lost" + service);
@@ -71,26 +81,31 @@ public class NsdHelper {
                     mService = null;
                 }
             }
+
             @Override
             public void onDiscoveryStopped(String serviceType) {
                 Log.i(TAG, "Discovery stopped: " + serviceType);
             }
+
             @Override
             public void onStartDiscoveryFailed(String serviceType, int errorCode) {
                 Log.e(TAG, "Discovery failed: Error code:" + errorCode);
             }
+
             @Override
             public void onStopDiscoveryFailed(String serviceType, int errorCode) {
                 Log.e(TAG, "Discovery failed: Error code:" + errorCode);
             }
         };
     }
+
     public void initializeResolveListener() {
         mResolveListener = new NsdManager.ResolveListener() {
             @Override
             public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
                 Log.e(TAG, "Resolve failed" + errorCode);
             }
+
             @Override
             public void onServiceResolved(NsdServiceInfo serviceInfo) {
                 boolean present = false;
@@ -102,19 +117,21 @@ public class NsdHelper {
                 mService = serviceInfo;
                 //quando ne trovo uno lo aggiungo all array
                 present = false;
-                for(NsdServiceInfo s : services){
-                    if(s.getServiceName().equals(serviceInfo.getServiceName())){
+                for (NsdServiceInfo s : services) {
+                    if (s.getServiceName().equals(serviceInfo.getServiceName())) {
                         present = true;
                         break;
                     }
                 }
                 if (!present) {
-                getServices().add(serviceInfo);}
+                    getServices().add(serviceInfo);
+                }
                 //qui ho risolto i servizi devo fare il display
                 //setto l'adapter
             }
         };
     }
+
     public void initializeRegistrationListener() {
         mRegistrationListener = new NsdManager.RegistrationListener() {
             @Override
@@ -122,36 +139,42 @@ public class NsdHelper {
                 registeredName = NsdServiceInfo.getServiceName();
                 Log.d(TAG, "Service registered: " + registeredName);
             }
+
             @Override
             public void onRegistrationFailed(NsdServiceInfo arg0, int arg1) {
                 Log.d(TAG, "Service registration failed: " + arg1);
             }
+
             @Override
             public void onServiceUnregistered(NsdServiceInfo arg0) {
                 Log.d(TAG, "Service unregistered: " + arg0.getServiceName());
             }
+
             @Override
             public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
                 Log.d(TAG, "Service unregistration failed: " + errorCode);
             }
         };
     }
+
     public void registerService(int port) {
         tearDown();  // Cancel any previous registration request
         initializeRegistrationListener();
-        NsdServiceInfo serviceInfo  = new NsdServiceInfo();
+        NsdServiceInfo serviceInfo = new NsdServiceInfo();
         serviceInfo.setPort(port);
         serviceInfo.setServiceName(mServiceName);
         serviceInfo.setServiceType(SERVICE_TYPE);
         mNsdManager.registerService(
                 serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
     }
+
     public void discoverServices() {
         stopDiscovery();  // Cancel any eFxisting discovery request
         initializeDiscoveryListener();
         mNsdManager.discoverServices(
                 SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
     }
+
     public void stopDiscovery() {
         if (mDiscoveryListener != null) {
             try {
@@ -161,9 +184,11 @@ public class NsdHelper {
             mDiscoveryListener = null;
         }
     }
+
     public NsdServiceInfo getChosenServiceInfo() {
         return mService;
     }
+
     public void tearDown() {
         if (mRegistrationListener != null) {
             try {
@@ -188,5 +213,50 @@ public class NsdHelper {
 
     public void setInit(int init) {
         this.init = init;
+    }
+
+
+    public class CustomResolveListener implements NsdManager.ResolveListener {
+        @Override
+        public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+            Log.e(TAG, "Resolve failed" + errorCode);
+            switch (errorCode) {
+                case NsdManager.FAILURE_ALREADY_ACTIVE:
+                    Log.e(TAG, "FAILURE_ALREADY_ACTIVE");
+                    // Just try again...
+                    mNsdManager.resolveService(serviceInfo, new CustomResolveListener());
+                    break;
+                case NsdManager.FAILURE_INTERNAL_ERROR:
+                    Log.e(TAG, "FAILURE_INTERNAL_ERROR");
+                    break;
+                case NsdManager.FAILURE_MAX_LIMIT:
+                    Log.e(TAG, "FAILURE_MAX_LIMIT");
+                    break;
+            }
+        }
+
+        @Override
+        public void onServiceResolved(NsdServiceInfo serviceInfo) {
+            boolean present = false;
+            Log.e(TAG, "Resolve Succeeded. " + serviceInfo);
+            if (serviceInfo.getServiceName().equals(registeredName)) {
+                Log.d(TAG, "Same IP.");
+                return;
+            }
+            mService = serviceInfo;
+            //quando ne trovo uno lo aggiungo all array
+            present = false;
+            for (NsdServiceInfo s : services) {
+                if (s.getServiceName().equals(serviceInfo.getServiceName())) {
+                    present = true;
+                    break;
+                }
+            }
+            if (!present) {
+                getServices().add(serviceInfo);
+            }
+            //qui ho risolto i servizi devo fare il display
+            //setto l'adapter
+        }
     }
 }
